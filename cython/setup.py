d25789cbc5f08bf2aa69550a2d4355c05e905287
Nicolau Werneck <nwerneck@gmail.com>
2012-05-11 16:45:50 -0300

    morpho madness

--- cython/setup.py
+++ cython/setup.py
@@ -5,9 +5,11 @@ from Cython.Distutils import build_ext
 setup(
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension("EMGMM", ["EMGMM.pyx"],
+                             include_dirs=['/usr/lib/python2.7/site-packages/numpy/core/include/'],
                              extra_objects=['schraudolph.o']
                              ),
-                   Extension("multivariateUtils", ["multivariateUtils.pyx"]
+                   Extension("multivariateUtils", ["multivariateUtils.pyx"],
+                             include_dirs=['/usr/lib/python2.7/site-packages/numpy/core/include/'],
                              )]
     
     )
